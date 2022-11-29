1556a3b243b499a0715e252ab3bcf6190d170b6f
Leonardo Rogelio <72422101+LeoRogelioSilva@users.noreply.github.com>
2022-11-28 23:10:57 -0300

    wip

--- Aula 11/Rota.py
+++ Aula 11/Rota.py
@@ -5,16 +5,16 @@ import random
 class Rota:
     
     def __init__(self):
-        self.listCoordenadas = []
+        self.coords = []
     
     def addCoord(self, coord):
-        self.listCoordenadas.append(coord)
+        self.coords.append(coord)
     
     def comprimento(self):
         comprimento = 0
-        for c in range(1,len(self.listCoordenadas)):
-            comprimento += self.listCoordenadas[c].distancia(self.listCoordenadas[c-1])
-        comprimento += self.listCoordenadas[-1].distancia(self.listCoordenadas[0])
+        for c in range(1,len(self.coords)):
+            comprimento += self.coords[c].distancia(self.coords[c-1])
+        comprimento += self.coords[-1].distancia(self.coords[0])
         return comprimento
 
     def copy(self):
@@ -23,21 +23,21 @@ class Rota:
 
     def __str__(self):
         strRota = ""
-        for a in self.listCoordenadas:
+        for a in self.coords:
             strRota += str(a)
             strRota += "->"
-        strRota += str(self.listCoordenadas[0])
+        strRota += str(self.coords[0])
         return strRota
 
     def shuffle(self):
-        random.shuffle(self.listCoordenadas)
+        random.shuffle(self.coords)
     
-    def getListCoordenadas(self):
-        return self.listCoordenadas
+    def getcoords(self):
+        return self.coords
 
     def otimiza(self):
         listaFinal = []
-        listaOriginal = self.getListCoordenadas()
+        listaOriginal = self.getcoords()
         listaFinal.append(listaOriginal[0])
         
         for i in range(1,len(listaOriginal)):
@@ -52,5 +52,12 @@ class Rota:
                         coodAlvo = listaOriginal[c]
             listaFinal.append(coodAlvo)
         
-        for a in range(len(self.listCoordenadas)):
-            self.listCoordenadas[a] = listaFinal[a]
+        for a in range(len(self.coords)):
+            self.coords[a] = listaFinal[a]
+    
+    def randomCoords(self, size = 1, maxValueCoord = 100):
+        for i in range(size):
+            n1 = random.randint(0, maxValueCoord)
+            n2 = random.randint(0, maxValueCoord)
+            coordenada = Coordenada((n1,n2))
+            self.addCoord(coordenada)
